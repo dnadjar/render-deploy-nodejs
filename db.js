4d2f4c907a85127f009914153d8efdc8b7f5e6f4
dnadjar <115176672+dnadjar@users.noreply.github.com>
2023-11-23 14:56:29 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  'postgres://db_user_p2sn_user:YCmT6zGOw2K3jOcZzHMKfdXVjkzqau3V@dpg-clflfc6f27hc739dgss0-a.frankfurt-postgres.render.com/db_user_p2sn', // TODO: database connection string
+  'postgres://db_user_p2sn_user:YCmT6zGOw2K3jOcZzHMKfdXVjkzqau3V@dpg-clflfc6f27hc739dgss0-a/db_user_p2sn', // TODO: database connection string
   {
     dialect: 'postgres',
     dialectOptions: {
